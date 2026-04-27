c29f49fd68dcd7f0cd2f29609036eb93c523f5a2
Muhammad Waqas <mw1438038@gmail.com>
2026-04-27 13:14:31 -0700

    Completed Python basics and OOP fundamentals

---
+++ Python_Files/forloop.py
@@ -0,0 +1,13 @@
+
+# for i in range(0,11):
+#     print('x'*20)
+#     print(f'This  is table of {i}')
+#     print('x'*20)
+# # #     for j in range(1,11):
+# # # #         print(f'{i} * {j} = {i*j}')
+ 
+# num=[1,2,3,4,5,6,7,8,9]
+sum=0
+for i in range():
+    sum = sum + i
+    print(f'sum of list is {sum}')
